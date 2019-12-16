70f8d378a12832a45624e0fa59d48aad6ae069c2
unknown <sankarpratapsamanta56@gmail.com>
2019-12-16 12:29:00 +0530

    update our code with withRouter for routing HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,29 +12,34 @@ class Directory extends React.Component{
             sections:[{
                 title:'HATS',
                 imageUrl:'https://images.pexels.com/photos/906002/pexels-photo-906002.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=200',
-                id:1
+                id:1,
+                linkUrl:'hats'
             },
             {
                 title:'JACKETS',
                 imageUrl:'https://i.ibb.co/px2tCc3/jackets.png',
-                id:2
+                id:2,
+                linkUrl:''
             },
             {
                 title:'SNEAKERS',
                 imageUrl:'https://images.pexels.com/photos/786003/pexels-photo-786003.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=200&w=200',
-                id:3
+                id:3,
+                linkUrl:''
             },
             {
                 title:'WOMENS',
                 imageUrl:'https://images.pexels.com/photos/1536619/pexels-photo-1536619.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=300&w=300',
                 size:'large',
-                id:4
+                id:4,
+                linkUrl:''
             },
             {
                 title:'MENS',
                 imageUrl:'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=300&w=300',
                 size:'large',
-                id:5
+                id:5,
+                linkUrl:''
             }]
         }
 
@@ -44,8 +49,8 @@ class Directory extends React.Component{
         return(
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title,imageUrl,id,size}) =>(
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    this.state.sections.map(({id,...otherSectionProps}) =>(
+                        <MenuItem key={id} {...otherSectionProps}/>
                     ))
                 }
             
